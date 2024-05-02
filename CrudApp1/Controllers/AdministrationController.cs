ec10b502ab6c8adfcc485e99b88a9107e65fa2f8
Taqi Ahmed <taqiahmed339@gmail.com>
2024-05-02 22:30:09 +0500

    Edit Role functionality added

--- CrudApp1/Controllers/AdministrationController.cs
+++ CrudApp1/Controllers/AdministrationController.cs
@@ -1,4 +1,5 @@
-﻿using CrudApp1.Models.Viewmodels;
+﻿using CrudApp1.Models;
+using CrudApp1.Models.Viewmodels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,10 +8,12 @@ namespace CrudApp1.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager,UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
         public IActionResult CreateRole()
         {
@@ -28,7 +31,7 @@ namespace CrudApp1.Controllers
                 IdentityResult res = await roleManager.CreateAsync(role);
                 if (res.Succeeded)
                 {
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("GetRoles", "Administration");
                 }
                 //add errors to modelstate
                 foreach(IdentityError error in res.Errors)
@@ -46,6 +49,69 @@ namespace CrudApp1.Controllers
             var roles = roleManager.Roles.ToList();
             return View(roles);
         }
+
+        //Edit Role
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+
+            var role = await roleManager.FindByIdAsync(id);
+            
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"Role With Id: {id} is not found";
+                return View("Error");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                Name = role.Name,
+            };
+
+            foreach(var user in userManager.Users.ToList())
+            {
+                if(await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.UsersInRole.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel roleViewModel)
+        {
+            var role = await roleManager.FindByIdAsync(roleViewModel.Id);
+            //if role is null then redirect to error page
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role With Id: {roleViewModel.Id} is not found";
+                return View("Error");
+            }
+            //else update the role
+            else
+            {
+                role.Name = roleViewModel.Name;
+                var res= await roleManager.UpdateAsync(role);
+
+                if (res.Succeeded)
+                {
+                    return RedirectToAction("GetRoles");
+                }
+
+                foreach(var error in res.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(roleViewModel);
+        }
+
 
     }
 }
---
+++ CrudApp1/Models/Viewmodels/EditRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudApp1.Models.Viewmodels
+{
+    public class EditRoleViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage ="Role Name is Required")]
+        
+        public string Name { get; set; }    
+
+        public List<string> UsersInRole { get; set; }= new List<string>();
+    }
+}
